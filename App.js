693e9a7afff14a9503f159cae7d3aef59331479c
Rishabh Sharma <rishabh.sharma@tevvo.com>
2024-07-25 02:08:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('h1', { id: 'child' }, 'This is heading 1'),
+  React.createElement('h2', { id: 'child' }, 'This is heading 2'),
+
+  React.createElement('h1', { id: 'child2' }, 'This is heading 1'),
+  React.createElement('h2', { id: 'child2' }, 'This is heading 2'),
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
